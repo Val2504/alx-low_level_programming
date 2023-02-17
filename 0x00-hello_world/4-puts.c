43854136692ab0b840b58ea5e079fc0330edd66e
Val2504 <valentineuchechukwu25@gmail.com>
2023-02-17 01:53:56 +0100

    fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/*
+ * this is a program that prints "Programming is like building a multilingual puzzle"
+ */
+
+#include<stdio.h>
+
+int main() {
+	putchar("Programming is like building a multilingual puzzle");
+
+	return 0;
+
