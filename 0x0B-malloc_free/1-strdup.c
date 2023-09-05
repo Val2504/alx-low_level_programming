bfbb02759be288fa166fd5757ee23f3132e0f660
Val2504 <valentineuchechukwu25@gmail.com>
2023-09-05 01:29:13 +0000

    updated the 2 task;

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,14 @@
 char *_strdup(char *str)
 {
 	char *tmp = NULL;
-	unsigned int len, n;
+	unsigned int len, n = 0;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	for (len = 0; str[len]; len++)/* gets the lenth of the string */
+	while (str[len])
+		len++;/* gets the lenth of the string */
 	tmp = malloc(sizeof(char) * (len + 1));
 
 	if (tmp == NULL)
@@ -26,9 +27,7 @@ char *_strdup(char *str)
 	for (n = 0; n < len + 1; n++)/* checks that the number */
 		/* of the element of the string */
 		/* is less than the length */
-	{
 		tmp[n] = str[n];
-	}
 	tmp[n] = '\0';
 
 	return (tmp);
