99b0acdcd53d3350f197034f77710f47e203494c
Val2504 <valentineuchechukwu25@gmail.com>
2023-09-12 00:52:43 +0000

    created a data structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+/** VALENTINE UCHECHUKWU UGWOKE **/
+
+This is a README file for 0x0E-structures_typedef deriectory
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - this is a data structure with three element
+ * @name: first element with a char* type
+ * @age: second element with a float type
+ * @owner: third element with a char* type
+ *
+ * Description: this is a data structure with two char pointer which
+ * is a string and a float data type
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
