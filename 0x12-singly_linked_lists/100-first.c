233e70cd5b64bb99660b36ef600d345be90d272f
Val2504 <valentineuchechukwu25@gmail.com>
2023-09-22 01:15:22 +0000

    prints some text

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main - print a some text
+ *
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
